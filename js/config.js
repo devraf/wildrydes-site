3e4224ec5db0b7a3c3303a328e2cfb2b64d450c7
devraf <rafael.perales@outlook.com>
2020-06-16 14:56:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
-};
+  cognito: {
+    userPoolId: 'us-east-2_i9PyUKTw1', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '1j0r0pnae1f3r4s8nd8o1uec07', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'us-east-2', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+}
